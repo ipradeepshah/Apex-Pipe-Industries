4e030f3863dcc75bb64383ae0e86e274637fb71e
Pradeep Shah <ipradeepshah@gmail.com>
2025-12-05 09:01:11 +0545

    added description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -18,41 +17,46 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Apex Pipe Industries",
-  description: " Apex Pipe Industries is one of the leading manufacturer of PVC Pipes in Nepal, having a track record of successfully manufacturing & marketing uPVC pipes conforming to different standards ranging from 1½ to 8",
+  description:
+    " Apex Pipe Industries is one of the leading manufacturer of PVC Pipes in Nepal, having a track record of successfully manufacturing & marketing uPVC pipes conforming to different standards ranging from 1½ to 8",
   keywords: [
     "Apex Pipe Industries",
     "Pipe Manufacturer in Nepal",
     "apex Pipe Industries Nepal",
   ],
-  authors: [{ name: "Pradeep Shah",
-              
-     url: "https://www.linkedin.com/in/ipradeepshah/" }],
+  authors: [
+    {
+      name: "Pradeep Shah",
+
+      url: "https://www.linkedin.com/in/ipradeepshah/",
+    },
+  ],
   creator: "Pradeep Shah",
   openGraph: {
     title: "Apex Pipe Industries",
-    description: " Apex Pipe Industries is one of the leading manufacturer of PVC Pipes in Nepal, having a track record of successfully manufacturing & marketing uPVC pipes conforming to different standards ranging from 1½ to 8",
+    description:
+      " Apex Pipe Industries is one of the leading manufacturer of PVC Pipes in Nepal, having a track record of successfully manufacturing & marketing uPVC pipes conforming to different standards ranging from 1½ to 8",
     url: "https://apexpipeindustries.com.np",
     siteName: "Apex Pipe Industries ",
-    
-    },
-  }
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (    
-        
-    <body className={`bg-white transition-colors dark:bg-gray-900 dark:text-white ${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider>
-          <Navbar />
-          <main className="min-h-screen pt-24">
-            {children}
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </body>
-    
+  return (
+    <html>
+        <body
+          className={`bg-white transition-colors dark:bg-gray-900 dark:text-white ${geistSans.variable} ${geistMono.variable}`}
+        >
+          <ThemeProvider>
+            <Navbar />
+            <main className="min-h-screen pt-24">{children}</main>
+            <Footer />
+          </ThemeProvider>
+        </body>
+        </html>
   );
 }
--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -5,102 +5,122 @@ export const projects: Project[] = [
     {
         
         title: 'Coupler',
+        description:'',
         technologies: [ '50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Coupler.png',
       },
       
       {
         title: 'Bend 87.5°',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/bend.png',
       },
       {
         title: 'Reducer',
+        description: '',
         technologies: ['75 x 50mm', '110 x 75mm', '160 x 110mm'],
         image: '/projects/Reducer.png',
       },
       {
         title: 'Metal Clamp',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/MEtal Clamp.png',
       },
       {
         title: 'Bend 87.5° with Door',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Bend-with-door.png',
       },
       {
         title: 'Cleaning Pipe',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Cleaning-pipe.png',
       },
       {
         title: 'Bend 45° ',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Bend-45.png',
       },
       {
         title: 'Single Tee ',
+        description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Single-tee.png',
       },
       {
          title: 'SP Trap ',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/SP-trap.png',
       },
       {
          title: 'Vent Cowl ',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Vent-cowl.png',
       },
       {
          title: 'Reducing Tee ',
+         description: '',
         technologies: ['100 x 75mm', '160 x 110mm'],
         image: '/projects/Reducing-tee.png',
       },
       {
          title: 'Single Tee with Door',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Single-tee-with-door.png',
       },
       {
          title: 'Double Tee',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Double-tee.png',
       },
       {
          title: 'Double Tee with Door',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Double-tee-with-door.png',
       },
       {
          title: 'Reducing Tee with Door',
+         description: '',
         technologies: ['110 x 75mm', '160 x 110mm'],
         image: '/projects/Reducing-tee-with-door.png',
       },
       {
          title: 'Multi Floor Trap 4"',
+         description: '',
         technologies: ['110 x 75mm', '160 x 110mm'],
         image: '/projects/Multi-floor-trap.png',
       },
       {
          title: 'Nahani Trap',
+         description: '',
         technologies: ['110 x 75mm', '160 x 110mm'],
         image: '/projects/Nahani-trap.png',
       },
       {
          title: 'Single Y',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Single-y.png',
       },
       {
          title: 'Single Y with Door',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/Single-y-with-door.png',
       },
       {
          title: 'End Cap',
+         description: '',
         technologies: ['50mm', '75mm', '110mm', '160mm'],
         image: '/projects/End-cap.png',
       }
