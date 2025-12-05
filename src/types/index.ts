5ee99b25656a330b89fe3462ae55d9cb69517e9f
Pradeep Shah <ipradeepshah@gmail.com>
2025-12-05 08:34:36 +0545

    updated interpafec type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,9 @@
 export interface Project {
   title: string;
-  description: string;
+  
   technologies: string[];
-  githubLink: string;
-  demoLink: string;
+  
   image: string;
 }
 
-export interface Blog {
-  title: string;
-  excerpt: string;
-  date: string;
-  readTime: string;
-  slug: string;
-}
+
